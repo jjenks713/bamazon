2d96bc10a701696f587889d0afc2ec3213bc4ce6
Chillen <josh.jenkin@live.com>
2019-02-02 18:23:31 -0700

    view inventory and view low inventory working

--- bamazon-manager.js
+++ bamazon-manager.js
@@ -20,45 +20,44 @@ connection.connect(function (err) {
 
 function manageOptions() {
     inquirer
-      .prompt({
-        name: "action",
-        type: "list",
-        message: "What would you like to do?",
-        choices: [
-          "View Products",
-          "View Low Inventory",
-          "Add to Inventory",
-          "Add New Product",
-          "exit"
-        ]
-      })
-      .then(function(answer) {
-        switch (answer.action) {
-        case "View Products":
-          viewProducts();
-          break;
-  
-        case "View Low Inventory":
-          viewInventory();
-          break;
-  
-        case "Add to Inventory":
-          addToInventory();
-          break;
-  
-        case "Add New Product":
-          addProduct();
-          break;
-            
-        case "exit":
-          connection.end();
-          break;
-        }
-      });
-  }
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "View Products",
+                "View Low Inventory",
+                "Add to Inventory",
+                "Add New Product",
+                "exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "View Products":
+                    viewProducts();
+                    break;
+
+                case "View Low Inventory":
+                    viewInventory();
+                    break;
+
+                case "Add to Inventory":
+                    addToInventory();
+                    break;
+
+                case "Add New Product":
+                    addProduct();
+                    break;
+
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
+}
 
 function viewProducts() {
-    console.log("testing");
     connection.query("SELECT * FROM bamazon_db.products;", function (err, res) {
         if (err) throw err;
         console.log("***************");
@@ -67,38 +66,53 @@ function viewProducts() {
         console.table(res);
         exitApp();
     });
-}
+};
 
-
-
-
-
-
-
-
-
-
-
-function exitApp(){
-    inquirer
-    .prompt({
-      name: "action",
-      type: "list",
-      message: "What would you like to do?",
-      choices: [
-        "Back to Menu",
-        "exit"
-      ]
-    })
-    .then(function(answer) {
-      switch (answer.action) {
-      case "Back to Menu":
-        manageOptions();
-        break;
-          
-      case "exit":
-        connection.end();
-        break;
-      }
+function viewInventory() {
+    connection.query("SELECT * FROM bamazon_db.products;", function (err, res) {
+        if (err) throw err;
+        console.log("\nThese are all the products that have inventory of 20 or less: \n")
+        for (var i = 0; i < res.length; i++) {
+            var lowInv = res[i].quantity;
+            var name = res[i].product;
+            if (lowInv <= 20) {
+                console.log(lowInv + " " + name + "\n" );
+            }
+        }
+        exitApp();
     });
+};
+
+
+
+
+
+
+
+
+
+
+
+function exitApp() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: "What would you like to do?",
+            choices: [
+                "Back to Menu",
+                "exit"
+            ]
+        })
+        .then(function (answer) {
+            switch (answer.action) {
+                case "Back to Menu":
+                    manageOptions();
+                    break;
+
+                case "exit":
+                    connection.end();
+                    break;
+            }
+        });
 };
